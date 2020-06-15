738150dd50c35559b35f4cea0092dad243fa72c2
Povilas Kulevicius <kulevicius.povilas@gmail.com>
2020-06-15 14:03:49 +0300

    Continuing work

--- Program.py
+++ Program.py
@@ -1,6 +1,9 @@
 import re
 import json
 import datetime
+import scipy.io.wavfile
+import numpy
+import os.path
 import Objects as o
 
 pathToBook = "C:/Users/Povilas Kulevicius/Desktop/VoiceRecognitionTools/SequenceMatcher2/Resources/Hobitas/Book/chapter-1.txt"
@@ -71,6 +74,8 @@ def cutWavFilesForTranscripts(wordDataFromJson, pathToWavFile, text, fileName):
     lastFileWasCut = True
     temporaryListOfWordsUsedForMatching = []
     wordsToMatchInWholeText = ""
+    cutStart = datetime.datetime.now()
+    fileNameIndex = 0
 
     i = 0
     while i < len(wordDataFromJson):
@@ -80,23 +85,64 @@ def cutWavFilesForTranscripts(wordDataFromJson, pathToWavFile, text, fileName):
             cutStart = getTimeSpanFromString(wordDataFromJson[i].startTime)
             wordsToMatchInWholeText = ""
             lastFileWasCut = False
+            fileNameIndex += 1
 
         temporaryListOfWordsUsedForMatching.append(wordDataFromJson[i])
         wordsToMatchInWholeText = wordDataFromJson[i] + ' '
         indexOfMatchedWordsInText = text.find(wordsToMatchInWholeText)
         if indexOfMatchedWordsInText != -1 & wordsToMatchInWholeText.split() >= 4:
+            cutAudioAndWriteToFile(text, wordsToMatchInWholeText, wordDataFromJson[i], cutStart, fileNameIndex)
 
 
 
+def cutAudioAndWriteToFile(wholeText, textToFind, lastWordData, audioStart, fileNameIndex):
+    transcript, remainingText = getTranscriptAndRemoveFromWholeText(wholeText, textToFind)
+    audioEnd = getTimeSpanFromString(lastWordData.endTime)
+    fileWasWriten = trimWavFile(audioStart, audioEnd, pathToWav, f'{cutTranscriptsName} 1_{fileNameIndex}_{transcript}.wav')
 
-def cutAudioAndWriteToFile():
+    if not fileWasWriten:
+        
+
+def getTranscriptAndRemoveFromWholeText(wholeText, textToFind):
+    indexOfFoundText = wholeText.find(textToFind) + len(textToFind.strip())
+    fileName = wholeText[:indexOfFoundText]
+
+    if len(wholeText.strip()) != len(textToFind.strip()):
+        remainingText = wholeText[:indexOfFoundText + 1]
+    else:
+        remainingText = ""
+
+    return fileName, remainingText
 
 
+def trimWavFile(audioStart, audioEnd, fileName, outputFileName):
+    fs1, y1 = scipy.io.wavfile.read(fileName)
+    l1 = numpy.array([[audioStart, audioEnd]])
+    l1 = numpy.ceil(l1 * fs1)  # get integer indices into the wav file - careful of end of array reading with a check for greater than y1.shape
+    newWavFileAsList = []
+    for elem in l1:
+        startRead = elem[0]
+        endRead = elem[1]
+        if startRead >= y1.shape[0]:
+            startRead = y1.shape[0] - 1
+        if endRead >= y1.shape[0]:
+            endRead = y1.shape[0] - 1
+        newWavFileAsList.extend(y1[startRead:endRead])
 
-def getTimeSpanFromString(startTimeString):
-    indexOfSCharacter = startTimeString.find('s')
-    startTimeString = startTimeString[:indexOfSCharacter]
-    return datetime.datetime.strptime(startTimeString, '%S.%f').time()
+    newWavFile = numpy.array(newWavFileAsList)
+
+    scipy.io.wavfile.write(outputFileName, fs1, newWavFile)
+
+    if os.path.isfile(fileName):
+        return True
+    else:
+        return False
+
+
+def getTimeSpanFromString(timeString):
+    indexOfSCharacter = timeString.find('s')
+    timeString = timeString[:indexOfSCharacter]
+    return datetime.datetime.strptime(timeString, '%S.%f').time()
 
 
 main()
